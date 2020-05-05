262226c6b50768f1e5faf9e3a1c7e0b45066dc77
marcoanaya <manaya@uchicago.edu>
2020-05-05 16:11:46 -0500

    Updates

---
+++ precache-manifest.dac2589ee6f4b8acff55745117f0eb63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce94ca6f1206d166f63b870c7fbc8d2",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "06c478e4169badd0796c",
+    "url": "/online-shop/static/css/main.ab894c22.chunk.css"
+  },
+  {
+    "revision": "3823d4fdeb0d06b12b73",
+    "url": "/online-shop/static/js/2.737d865b.chunk.js"
+  },
+  {
+    "revision": "e4d7c18cc4c58d8b735fa1eaf2f02446",
+    "url": "/online-shop/static/js/2.737d865b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c478e4169badd0796c",
+    "url": "/online-shop/static/js/main.3cc8a795.chunk.js"
+  },
+  {
+    "revision": "e8baff592bd6155080ce",
+    "url": "/online-shop/static/js/runtime-main.90a187ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3c42674e5ac22d054b9ee2b2c741c2f8.js"
+  "/online-shop/precache-manifest.dac2589ee6f4b8acff55745117f0eb63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/online-shop/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
