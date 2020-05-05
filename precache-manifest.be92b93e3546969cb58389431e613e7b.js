4133986f5772815dc6d85ae97cfcc111734b70e5
marcoanaya <manaya@uchicago.edu>
2020-05-05 16:01:28 -0500

    Updates

---
+++ precache-manifest.be92b93e3546969cb58389431e613e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607cb3c3c70e8cc79109c155a0c04cc2",
+    "url": "/online-shop/index.html"
+  },
+  {
+    "revision": "b9983b5b806c390b77b5",
+    "url": "/online-shop/static/css/main.ab894c22.chunk.css"
+  },
+  {
+    "revision": "3823d4fdeb0d06b12b73",
+    "url": "/online-shop/static/js/2.737d865b.chunk.js"
+  },
+  {
+    "revision": "e4d7c18cc4c58d8b735fa1eaf2f02446",
+    "url": "/online-shop/static/js/2.737d865b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9983b5b806c390b77b5",
+    "url": "/online-shop/static/js/main.34f5c9ce.chunk.js"
+  },
+  {
+    "revision": "e8baff592bd6155080ce",
+    "url": "/online-shop/static/js/runtime-main.90a187ac.js"
+  }
+]);
